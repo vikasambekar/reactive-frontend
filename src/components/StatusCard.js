0cf328184e3406434cfc62426d62106179bc06ba
Vikas Ambekar <ambekarviks@gmail.com>
2026-07-31 23:07:26 +0530

    Added the prop types for the Status card component

--- src/components/StatusCard.js
+++ src/components/StatusCard.js
@@ -1,8 +1,9 @@
-import { withLastUpdated } from "@/src/components/hoc/withLastUpdated";
+import PropTypes from 'prop-types'
+import {withLastUpdated} from "@/src/components/hoc/withLastUpdated";
 
-const StatusCard = ({ name, lastUpdated }) => {
+const StatusCard = ({name, lastUpdated}) => {
     return (
-        <div style={{ border: '10px solid #ccc', padding: '1rem' }}>
+        <div style={{border: '10px solid #ccc', padding: '1rem'}}>
             <h3>{name}</h3>
             <p>Last Updated: {lastUpdated}</p>
         </div>
@@ -10,5 +11,15 @@ const StatusCard = ({ name, lastUpdated }) => {
 };
 
 const EnhancedStatusCard = withLastUpdated(StatusCard);
+
+StatusCard.propTypes = {
+    name: PropTypes.string.isRequired,
+    lastUpdated: PropTypes.oneOfType(
+        [
+            PropTypes.string,
+            PropTypes.instanceOf(Date)
+        ]
+    ).isRequired,
+}
 
 export default EnhancedStatusCard;
